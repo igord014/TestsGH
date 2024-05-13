c7808e81e110a540cb4cb2dc9a9b4719d17dfb6e
igord014 <igord014@gmail.com>
2024-05-13 09:17:01 +0300

    s

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hil, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hils, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Presss the green button in the gutter to run the script.
